36d55aa237ba2fd40955ea0dee89ab9f882cacae
JAPHETH <japho0740@gmail.com>
2023-07-19 04:19:49 -0700

    Add the header file

---
+++ s_Shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+
+#endif
