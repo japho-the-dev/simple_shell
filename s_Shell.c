b21a94070a3aa379c736ca99da649a5a24b8e854
JAPHETH <japho0740@gmail.com>
2023-07-26 00:24:16 -0700

    Betty compliance

--- s_Shell.c
+++ s_Shell.c
@@ -1,52 +1,33 @@
 #include "s_Shell.h"
-/**
- * main - Entry point for the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- * stat: file status structure
- *
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-	char *inputLine = NULL;      /* Input line entered by the user */
-	char *commandArgs[1024];     /* Array to store command arguments */
-	size_t lineLength = 0;       /* Length of the input line */
-	size_t argIndex = 0;         /* Index for command arguments */
-	pid_t childPID;              /* PID of the child process */
-	struct stat fileStat;        /* File status structure */
-	ssize_t bytesRead;
 
+/**
+ * main - Entry point to the project
+ * @argc: no of command line arguments (void)
+ * @argv: Array of strings containing command-line args
+ * @env: Environmental variable
+ * Return: Always 0 (On success)
+ */
+int main(int argc, char *argv[], char **env)
+{
+	size_t x = 0;
+	char *s_line = NULL, *command;
+
+	(void)argc;
+	(void)argv;
+	(void)command;
 	while (1)
 	{
-		if (isatty(0))
-			printf("#cisfun ");
-		bytesRead = getline(&inputLine, &lineLength, stdin);
-		if (bytesRead == -1)
-			break;
-		/* Tokenize the input line into command arguments */
-		commandArgs[0] = strtok(inputLine, " \n");
-		for (argIndex = 1; commandArgs[argIndex - 1]; argIndex++)
-			commandArgs[argIndex] = strtok(NULL, " \n");
-		if (commandArgs[0])
-		{/* Check if the command executable exists */
-			if (stat(commandArgs[0], &fileStat) == 0)
-			{childPID = fork();
-				if (childPID == -1)
-				{
-					perror("Error: ");
-					return (-1);
-				}
-				if (childPID == 0)
-					execve(commandArgs[0], commandArgs, NULL);
-			}
-			else
-				printf("%s: No such file or directory\n", argv[0]);
+		display_prompt();
+
+		if (getline(&s_line, &x, stdin) == -1)
+			exit(EXIT_SUCCESS);
+		if (_strcmp(s_line, "exit\n") == 0)
+		{
+			free(s_line);
+			exit(0); /* Quit */
 		}
-		/* Wait for the child process to finish */
-		wait(&childPID);
+
+		execute_command(env, s_line, " \n\t\v\b\r\f");
 	}
-	free(inputLine);
-	(void)argc;
 	return (0);
 }
