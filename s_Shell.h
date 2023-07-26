2160620e5da3d15e088054b26fba36750830e7d3
JAPHETH <japho0740@gmail.com>
2023-07-26 02:46:02 -0700

    Addition of prototypes

--- s_Shell.h
+++ s_Shell.h
@@ -8,6 +8,16 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <errno.h>
 
-extern char **environ;
+/* Prototypes*/
+int main(int argc, char *argv[], char **env);
+void display_prompt(void);
+void print_number(int n);
+void custom_error(char *cmd);
+void execute_command(char **env, char *command_line, char *delimiter);
+char *_strcat(char *dest, char *src);
+char *path(char *cmd);
+int _strcmp(char *s1, char *s2);
+
 #endif
