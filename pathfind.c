2fb51921feb10ca28c945c0b8ded6d2699be3a74
JAPHETH <japho0740@gmail.com>
2023-07-26 01:52:33 -0700

    add a function to track path

---
+++ pathfind.c
@@ -0,0 +1,24 @@
+#include "s_Shell.h"
+
+/**
+ * path - Adds path prefix to the command
+ * @cmd: Command to prefix with path
+ * Return: the complete path
+ */
+char *path(char *cmd)
+{
+	char p1[50] = "/bin/";
+	char p2[50] = "/usr/bin/";
+	struct stat st;
+
+	_strcat(p1, cmd);
+	_strcat(p2, cmd);
+
+	if (stat(p1, &st) == 0)
+		return (strdup(p1));
+
+	else if (stat(p2, &st) == 0)
+		return (strdup(p2));
+
+	return (strdup(cmd));
+}
